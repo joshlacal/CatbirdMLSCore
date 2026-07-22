0fd8301799e197dba5ea02190646c8e6a6e4e17c
Josh LaCalamito <joshlacal@gmail.com>
2026-07-22 05:42:09 -0400

    deps: bump Petrel + PetrelCatbird exact pins to 1.0.4 (spec-tolerant unknown-field dispatch)

--- Package.swift
+++ Package.swift
@@ -17,8 +17,8 @@ let package = Package(
     ],
     dependencies: [
         .package(url: "https://github.com/groue/GRDB.swift.git", from: "7.0.0"),
-        .package(url: "https://github.com/joshlacal/Petrel.git", exact: "1.0.3"),
-        .package(url: "https://github.com/joshlacal/PetrelCatbird.git", exact: "1.0.3")
+        .package(url: "https://github.com/joshlacal/Petrel.git", exact: "1.0.4"),
+        .package(url: "https://github.com/joshlacal/PetrelCatbird.git", exact: "1.0.4")
     ],
     targets: [
         .target(
