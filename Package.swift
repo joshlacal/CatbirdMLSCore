3249e7ae7f9fe6b29875c3105cc5fb48837efe3b
Josh LaCalamito <joshlacal@gmail.com>
2026-08-18 12:07:56 -0400

    build(spm): accept Petrel 1.0.x (.upToNextMinor from 1.0.7)

--- Package.swift
+++ Package.swift
@@ -17,8 +17,8 @@ let package = Package(
     ],
     dependencies: [
         .package(url: "https://github.com/groue/GRDB.swift.git", from: "7.0.0"),
-        .package(url: "https://github.com/joshlacal/Petrel.git", .upToNextMinor(from: "1.1.0")),
-        .package(url: "https://github.com/joshlacal/PetrelCatbird.git", .upToNextMinor(from: "1.1.0"))
+        .package(url: "https://github.com/joshlacal/Petrel.git", .upToNextMinor(from: "1.0.7")),
+        .package(url: "https://github.com/joshlacal/PetrelCatbird.git", .upToNextMinor(from: "1.0.7"))
     ],
     targets: [
         .target(
